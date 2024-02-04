a82539738b5a408e0ec412d77f2b6abe44251c5b
dzaba1 <blazej.zabialowicz@gmail.com>
2024-02-04 12:04:25 +0100

    TestUtils

---
+++ src/Dzaba.TestUtils/TestExtensions.cs
@@ -0,0 +1,16 @@
+﻿using AutoFixture;
+using Dzaba.Utils;
+using Moq;
+
+namespace Dzaba.TestUtils;
+
+public static class TestExtensions
+{
+    public static Mock<T> FreezeMock<T>(this IFixture fixture)
+        where T : class
+    {
+        Require.NotNull(fixture, nameof(fixture));
+
+        return fixture.Freeze<Mock<T>>();
+    }
+}
---
+++ src/Dzaba.TestUtils/TestFixture.cs
@@ -0,0 +1,13 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+
+namespace Dzaba.TestUtils;
+
+public static class TestFixture
+{
+    public static IFixture Create()
+    {
+        return new Fixture()
+            .Customize(new AutoMoqCustomization());
+    }
+}
