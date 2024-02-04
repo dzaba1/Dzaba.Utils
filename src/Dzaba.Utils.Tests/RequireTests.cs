ae44dd4eb024487a0f96ba80f05eb2f5cc809559
dzaba1 <blazej.zabialowicz@gmail.com>
2024-02-04 11:56:35 +0100

    Central package version

--- src/Dzaba.Utils.Tests/RequireTests.cs
+++ src/Dzaba.Utils.Tests/RequireTests.cs
@@ -3,39 +3,38 @@ using FluentAssertions;
 using NUnit.Framework;
 using System;
 
-namespace Dzaba.CmdTemplate.Utils.Tests
+namespace Dzaba.CmdTemplate.Utils.Tests;
+
+[TestFixture]
+public class RequireTests
 {
-    [TestFixture]
-    public class RequireTests
+    [Test]
+    public void NotNull_WhenNull_ThenException()
     {
-        [Test]
-        public void NotNull_WhenNull_ThenException()
-        {
-            var name = "Test";
+        var name = "Test";
 
-            this.Invoking(s => Require.NotNull(null, name))
-                .Should().Throw<ArgumentNullException>().Where(e => e.ParamName == name);
-        }
+        this.Invoking(s => Require.NotNull(null, name))
+            .Should().Throw<ArgumentNullException>().Where(e => e.ParamName == name);
+    }
 
-        [TestCase(null)]
-        [TestCase("")]
-        public void NotEmpty_WhenEmpty_ThenException(string value)
-        {
-            var name = "Test";
+    [TestCase(null)]
+    [TestCase("")]
+    public void NotEmpty_WhenEmpty_ThenException(string value)
+    {
+        var name = "Test";
 
-            this.Invoking(s => Require.NotEmpty(value, name))
-                .Should().Throw<ArgumentNullException>().Where(e => e.ParamName == name);
-        }
+        this.Invoking(s => Require.NotEmpty(value, name))
+            .Should().Throw<ArgumentNullException>().Where(e => e.ParamName == name);
+    }
 
-        [TestCase(null)]
-        [TestCase("")]
-        [TestCase("   ")]
-        public void NotWhiteSpace_WhenWhiteSpace_ThenException(string value)
-        {
-            var name = "Test";
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("   ")]
+    public void NotWhiteSpace_WhenWhiteSpace_ThenException(string value)
+    {
+        var name = "Test";
 
-            this.Invoking(s => Require.NotWhiteSpace(value, name))
-                .Should().Throw<ArgumentNullException>().Where(e => e.ParamName == name);
-        }
+        this.Invoking(s => Require.NotWhiteSpace(value, name))
+            .Should().Throw<ArgumentNullException>().Where(e => e.ParamName == name);
     }
 }
