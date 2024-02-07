60e71493beab32868f14be9a7c493b7067d50901
dzaba1 <blazej.zabialowicz@gmail.com>
2024-02-07 12:52:20 +0100

    Configuration tests

---
+++ src/Dzaba.Utils.Configuration.Tests/ConfigurationExtensionsTests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Dzaba.Utils.Configuration.Tests;
+
+[TestFixture]
+public class ConfigurationExtensionsTests
+{
+    private IConfiguration CreateConfiguration()
+    {
+        return new ConfigurationBuilder()
+            .AddInMemoryCollection([
+                new KeyValuePair<string, string>("myString", "myString"),
+                new KeyValuePair<string, string>("Data:Name", "name"),
+                new KeyValuePair<string, string>("Data:Id", "1"),
+                ])
+            .Build();
+    }
+
+    [Test]
+    public void RegisterDzabaConfiguration_WhenConfigurationObject_ThenSingleton()
+    {
+        var services = new ServiceCollection();
+        var config = CreateConfiguration();
+        services.RegisterDzabaConfiguration(config);
+
+        using var provider = services.BuildServiceProvider();
+        var configuration1 = provider.GetRequiredService<IConfiguration>();
+        var configuration2 = provider.GetRequiredService<IConfiguration>();
+        configuration1.Should().BeSameAs(configuration2);
+    }
+
+    [Test]
+    public void RegisterDzabaConfiguration_WhenConfigurationBuilderAsSingleton_ThenSingleton()
+    {
+        var services = new ServiceCollection();
+        services.RegisterDzabaConfiguration(p => CreateConfiguration(), true);
+
+        using var provider = services.BuildServiceProvider();
+        var configuration1 = provider.GetRequiredService<IConfiguration>();
+        var configuration2 = provider.GetRequiredService<IConfiguration>();
+        configuration1.Should().BeSameAs(configuration2);
+    }
+
+    [Test]
+    public void RegisterDzabaConfiguration_WhenConfigurationBuilderAsTransient_ThenNewServices()
+    {
+        var services = new ServiceCollection();
+        services.RegisterDzabaConfiguration(p => CreateConfiguration());
+
+        using var provider = services.BuildServiceProvider();
+        var configuration1 = provider.GetRequiredService<IConfiguration>();
+        var configuration2 = provider.GetRequiredService<IConfiguration>();
+        configuration1.Should().NotBeSameAs(configuration2);
+    }
+
+    [Test]
+    public void RegisterDzabaSettings_WhenSectionExists_ThenModel()
+    {
+        var services = new ServiceCollection();
+        var config = CreateConfiguration();
+        services.RegisterDzabaConfiguration(config);
+        services.RegisterDzabaSettings<TestSettings>("Data");
+
+        using var provider = services.BuildServiceProvider();
+        var settings = provider.GetRequiredService<TestSettings>();
+        settings.Name.Should().Be("name");
+        settings.Id.Should().Be(1);
+    }
+
+    private class TestSettings
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
--- src/Dzaba.Utils.Configuration/ConfigurationExtensions.cs
+++ src/Dzaba.Utils.Configuration/ConfigurationExtensions.cs
@@ -2,44 +2,43 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
-namespace Dzaba.Utils.Configuration
+namespace Dzaba.Utils.Configuration;
+
+public static class ConfigurationExtensions
 {
-    public static class ConfigurationExtensions
+    public static void RegisterDzabaSettings<T>(this IServiceCollection services, string sectionName)
+        where T : class
     {
-        public static void RegisterDzabaSettings<T>(this IServiceCollection services, string sectionName)
-            where T : class
-        {
-            Require.NotNull(services, nameof(services));
-            Require.NotWhiteSpace(sectionName, nameof(sectionName));
+        Require.NotNull(services, nameof(services));
+        Require.NotWhiteSpace(sectionName, nameof(sectionName));
 
-            services.AddTransient<T>(p =>
-            {
-                var config = p.GetRequiredService<IConfiguration>();
-                var section = config.GetSection(sectionName);
-                return section.Get<T>();
-            });
+        services.AddTransient<T>(p =>
+        {
+            var config = p.GetRequiredService<IConfiguration>();
+            var section = config.GetSection(sectionName);
+            return section.Get<T>();
+        });
+    }
+
+    public static void RegisterDzabaConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        Require.NotNull(configuration, nameof(configuration));
+
+        services.AddSingleton<IConfiguration>(configuration);
+    }
+
+    public static void RegisterDzabaConfiguration(this IServiceCollection services, Func<IServiceProvider, IConfiguration> configurationBuilder,
+        bool singleton = false)
+    {
+        Require.NotNull(configurationBuilder, nameof(configurationBuilder));
+
+        if (singleton)
+        {
+            services.AddSingleton<IConfiguration>(configurationBuilder);
         }
-
-        public static void RegisterDzabaConfiguration(this IServiceCollection services, IConfiguration configuration)
+        else
         {
-            Require.NotNull(configuration, nameof(configuration));
-
-            services.AddSingleton<IConfiguration>(configuration);
-        }
-
-        public static void RegisterDzabaConfiguration(this IServiceCollection services, Func<IServiceProvider, IConfiguration> configurationProvider,
-            bool singleton = false)
-        {
-            Require.NotNull(configurationProvider, nameof(configurationProvider));
-
-            if (singleton)
-            {
-                services.AddSingleton<IConfiguration>(configurationProvider);
-            }
-            else
-            {
-                services.AddTransient<IConfiguration>(configurationProvider);
-            }
+            services.AddTransient<IConfiguration>(configurationBuilder);
         }
     }
 }
