025a17eb57a117a8b1d89dc57011f6cd45c3bafc
dzaba1 <blazej.zabialowicz@gmail.com>
2026-05-13 20:50:47 +0200

    Example service

---
+++ src/Dzaba.ExampleService/Controllers/JwtController.cs
@@ -0,0 +1,63 @@
+﻿using Dzaba.AspNetUtils;
+using Dzaba.AspNetUtils.ActionFilters;
+using Dzaba.Utils.AspNet;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace Dzaba.ExampleService.Controllers;
+
+[Route("jwt")]
+[HandleErrors]
+public class JwtController : ControllerBase
+{
+    private readonly ILogger<JwtController> logger;
+
+    public JwtController(ILogger<JwtController> logger)
+    {
+        ArgumentNullException.ThrowIfNull(logger);
+
+        this.logger = logger;
+    }
+
+    [HttpPost]
+    public string Create([Required, FromBody]JwtTokenToCreate settings)
+    {
+        logger.LogInformation("Start building JWT token.");
+
+        var builder = new JwtTokenBuilder()
+            .WithAudience(settings.Settings.Audience)
+            .WithIssuer(settings.Settings.Authority)
+            .WithLifetime(settings.Lifetime)
+            .WithSecurityKey(settings.Settings.IssuerSigningKey);
+
+        if (settings.Roles != null)
+        {
+            foreach (var role in settings.Roles)
+            {
+                builder.WithClaim(ClaimTypes.Role, role);
+            }
+        }
+
+        return builder.Build().EncodeToString();
+    }
+
+    [HttpGet("check")]
+    [Authorize()]
+    public bool CheckNoRoles()
+    {
+        logger.LogInformation("Checking no roles");
+
+        return true;
+    }
+
+    [HttpGet("check/testRole")]
+    [Authorize(Roles = "TestRole")]
+    public bool CheckTestRole()
+    {
+        logger.LogInformation("Check TestRole");
+
+        return true;
+    }
+}
---
+++ src/Dzaba.ExampleService/JwtTokenToCreate.cs
@@ -0,0 +1,14 @@
+﻿using Dzaba.Utils.AspNet;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dzaba.ExampleService;
+
+public sealed class JwtTokenToCreate
+{
+    [Required]
+    public JwtSettings Settings { get; set; }
+
+    public TimeSpan Lifetime { get; set; } = TimeSpan.FromMinutes(30);
+
+    public string[] Roles { get; set; }
+}
